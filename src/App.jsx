486fa33e2b9f5d92c1a176924c9cd3cf1bee84d8
hoang.ausway <hoang.ausway@gmail.com>
2020-11-22 14:10:28 +1100

    first

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Header from './Header'
+import './index.css'
+
+const App = () => (
+  <div>
+    <Header />
+    <div>I'm the header app</div>
+  </div>
+)
+
+ReactDOM.render(<App />, document.getElementById('app'))
